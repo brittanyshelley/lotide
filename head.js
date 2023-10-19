f5d1c7b250f2dffb9fdff56aa5c911376c4d25a2
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-19 00:48:18 +0000

    completed code for head

--- head.js
+++ head.js
@@ -9,3 +9,16 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+const head = function(array) {
+  if (array.length > 0) {
+    return array[0];
+  } else {
+    return undefined;
+  }
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
