b67a0d3ac6a53247dc64a0d0a87d7b5f227fe0fd
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-20 04:22:30 +0000

    tail function

--- tail.js
+++ tail.js
@@ -10,8 +10,11 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
+const tail = function(arr) {
+  arr.toSpliced(1);
+};
 
-
+console.log(tail);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
