3d5be5d9230cb4f9cd634160473ca3bbb1dabdb7
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-22 20:54:24 -0600

    linted

--- countLetters.js
+++ countLetters.js
@@ -1,22 +1,22 @@
 const countLetters = function(word) {
-	// Create the object which will store the occurrences
-	const count = {};
+  // Create the object which will store the occurrences
+  const count = {};
 
-	// Loop over letters
-	for (let i = 0; i < word.length; i++) {
-		const letter = word[i];
+  // Loop over letters
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
 
-		// if the property isn't in the object, it means that the letter is a new one and we'll create this property and assign it the value 1
-		if (!count[letter]) {
-			count[letter] = 1;
+    // if the property isn't in the object, it means that the letter is a new one and we'll create this property and assign it the value 1
+    if (!count[letter]) {
+      count[letter] = 1;
 
-			// else, the property is already there and we just increment it by 1
-		} else {
-			count[letter]++;
-		}
-	}
+      // else, the property is already there and we just increment it by 1
+    } else {
+      count[letter]++;
+    }
+  }
 
-	return count;
+  return count;
 };
 
 
