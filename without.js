c3bcc5539dac2517f9f0f3dadedc286556714422
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-22 20:37:10 -0600

    added assertion

--- without.js
+++ without.js
@@ -5,7 +5,7 @@ const without = function(source, itemsToRemove) {
       newArray.push(element);
     }
   }
-  console.log(newArray);
+  return newArray;
 };
 
 without([1, 2, 3], [1]) // => [2, 3]
@@ -38,3 +38,4 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
