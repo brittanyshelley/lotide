464a548b6b631bcd472f0302ad9f665b4f85faa1
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-22 18:05:06 -0600

    initial commit

---
+++ without.js
@@ -0,0 +1,12 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  else {
+    //comparing elements in arrays
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
