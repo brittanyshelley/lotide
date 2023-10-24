1727f84c9407678e8f237637a99c690889c28fa9
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-24 17:05:59 -0600

    eslint

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,32 +1,29 @@
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false; 
+    return false;
   }
-  for (const key of Object.keys(object1)) { 
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { 
-      if (!eqArrays(object1[key], object2[key])) { 
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     }
     if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
-      if (object1[key] instanceof Object && object2[key] instanceof Object) {
-        if (!eqObjects(object1[key], object2[key])) {
-          return false;
-        }
+      if (object1[key] !== object2[key]) {
+        return false;
       }
-    }
-    else {
-      if (object1[key] !== object2[key]) { 
+    } else {
+      if (object1[key] !== object2[key]) {
         return false;
       }
     }
-  } 
+  }
   return true;
 };
 
 const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+const anotherShirtObject = { size: "medium", color: "red" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 
 const assertObjectsEqual = function(object1, object2) {
   const inspect = require('util').inspect;
