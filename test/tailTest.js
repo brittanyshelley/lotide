99c560eea6737164486bf41f9972406c47b476de
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-25 19:11:20 +0000

    fixed assertion

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,12 @@
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
+const words = ["Lighthouse", "Labs", "Bootcamp"];
 const result = tail(words);
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
