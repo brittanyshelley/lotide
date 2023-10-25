21b578a19464131e08993598c9f4cf3a377bfddb
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-25 10:55:58 -0600

    fixed test

--- tail.js
+++ tail.js
@@ -10,7 +10,6 @@ const tail = function(arr) {
 
 module.exports = tail;
 
-console.log(tail);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+
