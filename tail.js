6f0fa9de70bba3a44ebc63772e03b27a8389551b
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-25 10:59:45 -0600

    fixed according to AI

--- tail.js
+++ tail.js
@@ -1,11 +1,7 @@
 const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
-  let newArr = [];
-  for (let i = 1; i < arr.length; i++) {
-    newArr.push(arr[i]);
-  }
-  return newArr;
+  return arr.slice(1);
 };
 
 module.exports = tail;
