b5a294ed00ba7e43a9074ae0eed054845b6995ff
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-25 10:38:07 -0600

    module exports

--- tail.js
+++ tail.js
@@ -1,18 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
-  arr.toSpliced(1);
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 };
+
+module.exports = tail;
 
 console.log(tail);
 const words = ["Yo Yo", "Lighthouse", "Labs"];
