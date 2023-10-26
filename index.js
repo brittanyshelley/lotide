d5032fcbc366b9e45f1e665b6d6e9c87887279a1
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-25 21:36:14 -0600

    removed map

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require("./countOnly");
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 const eqObjects = require('./eqObjects');
@@ -18,21 +17,20 @@ const flatten = require('./flatten');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  assertEqual: assertEqual,
-  flatten: flatten
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  letterPositions,
+  takeUntil,
+  without,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  assertEqual,
+  flatten
 };
